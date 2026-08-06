11ea29ce97033702e03ee690d3eeac986b2da282
Aashutosh-kc <aashutoshkc853@gmail.com>
2026-08-06 21:18:05 +0545

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import {useState} from "react"
+import './App.css';
+import Timer from "./components/TimerDisplay";
+function App(){
+  return (
+    <>
+      <Timer />
+    </>
+  )
+}
+export default App;
---
+++ src/components/TimerDisplay.jsx
@@ -0,0 +1,48 @@
+import { useState,useEffect } from 'react';
+export default function Timer(){
+
+    const [timeleft,setTimeLeft] = useState(1500);
+    const [isRunning,setIsRunning] = useState(false);
+
+    function getFormattedTime(time){
+        const mins = Math.floor(time / 60);
+        const seconds = time % 60;
+        const format = mins + " : " + seconds.toString().padStart(2,"0");
+        return format;
+    }
+    useEffect(() =>{
+        if (!isRunning) return;
+        const id = setInterval(()=>{
+            setTimeLeft((prev) => {if (prev == 0){
+                return 0;
+            }
+        else{
+            return prev - 1;
+        }})
+            
+        },1000)
+        return () => clearInterval(id);
+    }
+        ,[isRunning]);
+        
+    useEffect(() => {
+    if (timeleft === 0) {
+        setIsRunning(false);
+    }
+}, [timeleft]);
+    return(
+        <>
+        <p >{getFormattedTime(timeleft)}</p>
+        <button onClick={()=>setIsRunning((isRunning)=> !isRunning)}>
+            {isRunning? "Stop" : "Start"}
+        </button>
+
+        <button onClick={()=>{
+        setTimeLeft((prev) =>  1500);
+        setIsRunning(false);
+        }}>
+            Reset
+        </button>
+        </>
+    )
+}
