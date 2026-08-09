c0e5276dc1e4f0e74a9ac2775dcc834845060b3a
Aashutosh-kc <aashutoshkc853@gmail.com>
2026-08-09 20:36:58 +0545

    feat(timer): add short and long break timer modes
    Refactor timer into reusable component that accepting time and title props, all three timer share one implementation instead of seperate.

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,22 @@ import './App.css';
 import './index.css'
 import Timer from "./components/TimerDisplay";
 function App(){
+
+  const [timer,setTimer] = useState("Pomodorro");
+
   return (
-    <div className="bg">
-      <Timer />
+    <div className="all-timer">
+
+      <div className="timer-selector">
+        <button onClick={()=> setTimer("Pomodorro")}>Pomdorro</button>
+        <button onClick={()=> setTimer("Short Break")}>Short Break</button>
+        <button onClick={()=> setTimer("Long Break")}>Long Break</button>
+      </div>
+
+      { timer === "Pomodorro" && <Timer time={1500} title={"Pomodorro"}/>}
+      { timer === "Short Break" && <Timer time={300} title={"Short Break"}/>}
+      { timer === "Long Break" && <Timer time={600} title={"LongBreak"}/>}
+
     </div>
   )
 }
--- src/components/TimerDisplay.jsx
+++ src/components/TimerDisplay.jsx
@@ -1,52 +1,51 @@
-import { useState,useEffect } from 'react';
-import './TimerDisplay.css';
-export default function Timer(){
+import {useState,useEffect } from "react";
+import './TimerDisplay.css'
 
-    const [timeleft,setTimeLeft] = useState(1500);
+export default function Timer({time,title}){
+
+    const [timeleft,setTimeleft] = useState(time);
     const [isRunning,setIsRunning] = useState(false);
 
-    function getFormattedTime(time){
-        const mins = Math.floor(time / 60);
-        const seconds = time % 60;
-        const format = mins + " : " + seconds.toString().padStart(2,"0");
-        return format;
-    }
-    useEffect(() =>{
+    useEffect(()=>{
         if (!isRunning) return;
-        const id = setInterval(()=>{
-            setTimeLeft((prev) => {if (prev == 0){
-                return 0;
-            }
-        else{
-            return prev - 1;
-        }})
-            
+        const timeInterval = setInterval(()=>{
+            setTimeleft((prev) => prev-1);
         },1000)
-        return () => clearInterval(id);
+
+        return () => clearInterval(timeInterval)
+
+    },[isRunning]);
+
+    function getFormattedTime(unformattedtime){
+        const min = Math.floor(unformattedtime / 60);
+        const sec = unformattedtime % 60;
+        const FormattedTime = min.toString() + " : " + sec.toString().padStart(2,"0");
+        return FormattedTime;
     }
-        ,[isRunning]);
 
     useEffect(() => {
-    if (timeleft === 0) {
-        setIsRunning(false);
-    }
-}, [timeleft]);
-    return(
-        <div className='timer-card'>
-        <h2 className='heading'>Pomodorro</h2>
-        <p className='time'>{getFormattedTime(timeleft)}</p>
-        <div className='btn'>
-        <button className="start-btn"onClick={()=>setIsRunning((isRunning)=> !isRunning)}>
-            {isRunning? "Stop" : "Start"}
-        </button>
+        if(timeleft == 0){
+            setIsRunning((prev) => (false))
+        }
+    },[timeleft]);
 
-        <button className="reset-btn"onClick={()=>{
-        setTimeLeft((prev) =>  1500);
-        setIsRunning(false);
-        }}>
-            Reset
-        </button>
-        </div>
+    return(
+        <div className="timer-card">
+            <h2>{title}</h2>
+            <p>{getFormattedTime(timeleft)}</p>
+            <div className="main-btns">
+                <button className="start-btn" onClick={()=> {setIsRunning((prev)=> (!prev))}}>
+                    {isRunning?"Stop": "Start"}
+                </button>
+                <button className="reset-btn" onClick={()=>{
+                    setTimeleft((prev) => time);
+                    setIsRunning(false);
+                }}>
+                Reset
+                </button>
+            </div>
+        
         </div>
     )
+
 }
