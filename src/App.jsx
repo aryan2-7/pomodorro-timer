274c4fe9ea14d2fa53d57ab3bfceda4a96e70033
Aashutosh-kc <aashutoshkc853@gmail.com>
2026-08-09 21:41:17 +0545

    style(timer): add proper button styling

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,14 @@ function App(){
     <div className="all-timer">
 
       <div className="timer-selector">
-        <button onClick={()=> setTimer("Pomodorro")}>Pomdorro</button>
-        <button onClick={()=> setTimer("Short Break")}>Short Break</button>
-        <button onClick={()=> setTimer("Long Break")}>Long Break</button>
+        <button className={timer === "Pomodorro"?"selected":"unselected"} onClick={()=> setTimer("Pomodorro")}>Pomdorro</button>
+        <button className={timer === "Short Break"?"selected":"unselected"} onClick={()=> setTimer("Short Break")}>Short Break</button>
+        <button className={timer === "Long Break"?"selected":"unselected"}onClick={()=> setTimer("Long Break")}>Long Break</button>
       </div>
 
       { timer === "Pomodorro" && <Timer time={1500} title={"Pomodorro"}/>}
       { timer === "Short Break" && <Timer time={300} title={"Short Break"}/>}
-      { timer === "Long Break" && <Timer time={600} title={"LongBreak"}/>}
+      { timer === "Long Break" && <Timer time={600} title={"Long Break"}/>}
 
     </div>
   )
